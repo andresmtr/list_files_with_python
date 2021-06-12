2775ac2368a4876086b9a00fc6a70ca9b88bea7f
Andres <31396408+andresmtr@users.noreply.github.com>
2021-06-11 20:29:30 -0500

    final py document

--- Listar_python.py
+++ Listar_python.py
@@ -1,17 +1,21 @@
-## first install conda install -c conda-forge xlsxwriter and conda install -c anaconda openpyxl
+## the next imports 
+
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
 
+## include path of file to list
 
-def ls(ruta = '/Users/andresmauriciotrianareina/Downloads/Actas de entrega participantes'):
+def ls(ruta = 'c://documents'):
     ls.para = [arch for arch in listdir(ruta) if isfile(join(ruta, arch))]
     #print(ls.para)
 
-    df = pd.DataFrame(ls.para, columns=['cedulas'])
+    df = pd.DataFrame(ls.para, columns=['name_column'])
 
-    print(df['cedulas'])
+    print(df['name_column'])
     
-    df.to_excel('cedulas.xlsx', sheet_name='cedulas')
+    ## create excel with the information
+
+    df.to_excel('name_excel.xlsx', sheet_name='name_sheet')
 
 print(ls())
